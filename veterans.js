1c0e47b3e693f216b87422d482d7c7571d5d3c88
zayyaminyin <64671647+zayyaminyin@users.noreply.github.com>
2020-05-01 21:55:55 -0400

    Add files via upload

---
+++ veterans.js
@@ -0,0 +1,21 @@
+var vaSiteClick = document.getElementById("vaSite")
+vaSiteClick.onclick = function () {
+        location.href = "https://www.va.gov/";
+    };
+
+var retireClick = document.getElementById("retire")
+retireClick.onclick = function () {
+        location.href = "file:///C:/Users/user/Desktop/veterans.html";
+    };
+
+// var modeButtons = document.querySelectorAll(".mode")
+// function setUpModeButtons(){
+// 	for(var i = 0; i < modeButtons.length; i++){
+// 	modeButtons[i].addEventListener("hover", function(){
+// 		modeButtons[0].classList.remove("selected")
+// 		modeButtons[1].classList.remove("selected")
+// 		modeButtons[2].classList.remove("selected")
+// 		this.classList.add("selected");
+// 	});
+//   }
+// }
---
+++ veteransHome.js
@@ -0,0 +1,4 @@
+var vaSiteClick = document.getElementById("start")
+vaSiteClick.onclick = function () {
+        location.href = "file:///C:/Users/user/Desktop/veterans.html";
+    };
